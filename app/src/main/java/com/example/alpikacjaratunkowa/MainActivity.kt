fef9e8d7edbe45f9baa460284528faa6387c66e1
Areem <98659406+RavMRafiki@users.noreply.github.com>
2024-01-27 18:56:53 +0100

    Added Location

--- app/src/main/java/com/example/alpikacjaratunkowa/MainActivity.kt
+++ app/src/main/java/com/example/alpikacjaratunkowa/MainActivity.kt
@@ -1,22 +1,33 @@
 package com.example.alpikacjaratunkowa
 
+import PermissionUtils
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.appcompat.app.AppCompatActivity
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
 
 class MainActivity : AppCompatActivity(),SensorEventListener {
 
     private lateinit var sensorManager: SensorManager
     private val acceletometer: Sensor? = null
     private val gyroscope: Sensor? = null
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var accelerometerValues: TextView
     private lateinit var gyroscopeValues: TextView
+    private lateinit var gpsValues: TextView
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,10 +36,12 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
 
         accelerometerValues = findViewById(R.id.accelerometrValues)
         gyroscopeValues = findViewById(R.id.gyroscopeValues)
+        gpsValues = findViewById(R.id.gpsValues)
 
         val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         val accelerometer: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        val  gyroscope: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+        val gyroscope: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+        val gps = LocationServices.getFusedLocationProviderClient(this)
 
         if (accelerometer != null){
             sensorManager.registerListener(this,accelerometer,SensorManager.SENSOR_DELAY_NORMAL)
@@ -41,7 +54,31 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
         }else{
             gyroscopeValues.text = "No gyroscope found"
         }
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Log.d("NOHOMO","NOHOMO")
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf<String>(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ),
+                1
+            )
+            return
+        }
+        gps.lastLocation.addOnSuccessListener { location: Location? ->
+            gpsValues.text = location.toString()
+        }
     }
+
     @SuppressLint("SetTextI18n")
     override fun onSensorChanged(event: SensorEvent) {
         if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
@@ -63,6 +100,7 @@ class MainActivity : AppCompatActivity(),SensorEventListener {
     }
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        println("Implemented onAccuracyChanged MainAcitvieit")
     }
 
     override fun onDestroy() {
